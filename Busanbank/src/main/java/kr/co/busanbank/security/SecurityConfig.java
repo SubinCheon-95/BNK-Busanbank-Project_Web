bf08fd3827b45ba26c5592d69165c5e07b2acefe
Shalline <akdbtkfk@naver.com>
2025-12-19 18:03:00 +0900

    [25/12/19] newsAI/update/by shasha

--- Busanbank/src/main/java/kr/co/busanbank/security/SecurityConfig.java
+++ Busanbank/src/main/java/kr/co/busanbank/security/SecurityConfig.java
@@ -100,12 +100,16 @@ public class SecurityConfig {
                         .requestMatchers("/api/flutter/branches/**").permitAll()  // 지점목록
                         .requestMatchers("/api/flutter/employees").permitAll()  // 직원
                         .requestMatchers("/api/flutter/news/**").permitAll() // 뉴스분석
+                        .requestMatchers("/api/flutter/categories").permitAll()
+                        .requestMatchers("/api/flutter/products/by-category/**").permitAll()
+                        .requestMatchers("/api/flutter/news/**").permitAll()
 
                         // ✅ 로그인 필요한 API (JWT 인증) 25/12/15 수진
                         .requestMatchers("/api/flutter/coupons/**").hasRole("USER")  // 쿠폰 조회
                         .requestMatchers("/api/flutter/points/**").hasRole("USER")  // 포인트 조회
                         .requestMatchers("/api/flutter/join/**").hasRole("USER")  // 상품 가입
                         .requestMatchers("/api/flutter/verify/**").hasRole("USER")  // 계좌 비번 비교
+
 
 
                         // 비트코인/금/오일 api 25/12/16 윤종인
